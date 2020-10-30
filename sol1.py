6c4383aebfe73c906d1effa4340cbda93a69f592
Mohammad Saleh <mohammad.saleh2@cs.huji.ac.il>
2020-10-30 16:40:31 +0200

    rgb2yiq ?

--- sol1.py
+++ sol1.py
@@ -3,6 +3,9 @@ import skimage.color as sk
 import imageio as io
 import matplotlib.pyplot as plt
 
+transMat = [[0.299, 0.587, 0.114], [0.596, -0.275, -0.321], [0.212, -0.523, 0.311]]
+tMatrix = np.array(transMat)
+
 
 def read_image(filename, representation):
     im = io.imread(filename)
@@ -17,16 +20,22 @@ def read_image(filename, representation):
         return im_g
 
     elif representation == 2:
-        # print(im_float)
+        print(im_float)
         # print(im_float.shape)
         # print(im_float.dtype)
         return im_float
 
 
 def imdisplay(filename, representation):
-    im = read_image(filename,representation)
-    plt.imshow(im,cmap="gray")
+    im = read_image(filename, representation)
+    plt.imshow(im, cmap="gray")
     plt.show()
 
+
+# def rgb2yiq(imRGB):
+
+
+
 if __name__ == '__main__':
-    imdisplay("image.jpeg",1)
+    # imdisplay("image.jpeg",1)
+    x = read_image("image.jpeg", 2)
