2d6e0ba854c9f8835e46db5269fc5c38683d2d0c
Mohammad Saleh <mohammad.saleh2@cs.huji.ac.il>
2020-10-30 13:58:14 +0200

    read_image implem

--- sol1.py
+++ sol1.py
@@ -1,8 +1,29 @@
 import numpy as np
-import  skimage.color as sk
+import skimage.color as sk
+import imageio as io
+
 
 def read_image(filename, representation):
-    if representation == 1:
-        
-    if representation == 2:
+    im = io.imread(filename)
+    im_float = im.astype(np.float64)
+    im_float /= 255
 
+    if representation == 1:
+        im_g = sk.rgb2gray(im_float)
+        # print(im_g)
+        # print(im_g.shape)
+        # print(im_g.dtype)
+        return im_g
+
+    elif representation == 2:
+        # print(im_float)
+        # print(im_float.shape)
+        # print(im_float.dtype)
+        return im_float
+
+
+
+
+
+if __name__ == '__main__':
+    read_image("image.jpeg", 1)
